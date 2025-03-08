b688dc7da266805e0e0c9105c27709060901afba
emanuelsoares97 <tutasoares17@gmail.com>
2025-03-08 11:40:46 +0000

    teste com biblio ctypes

--- teste.py
+++ teste.py
@@ -1,21 +1,21 @@
-import pyautogui
-import time
-import pydirectinput
+import ctypes
+from time import sleep
+def clicar_mouse(x, y):
+    ctypes.windll.user32.SetCursorPos(x, y)  # Move o cursor para a posição
+    ctypes.windll.user32.mouse_event(2, 0, 0, 0, 0)  # Pressiona botão esquerdo
+    ctypes.windll.user32.mouse_event(4, 0, 0, 0, 0)  # Solta botão esquerdo
+
+sleep(1)
+clicar_mouse(1321,375)
 
 
 
-time.sleep(0.5)
 
 
-pyautogui.moveTo(1178, 457)
-
-time.sleep(1)
-pydirectinput.click()
 
 
-import pygetwindow as gw
-janela = gw.getWindowsWithTitle("Metin2Portugalia")  # Nome da janela do jogo
-if janela:
-    janela[0].activate()  # Ativa a janela
-time.sleep(0.5)  # Pequeno delay para garantir que a janela esteja ativa
-pydirectinput.click()
+
+
+
+
+
